f2f40a3ac52a54402fe8698014662369dd4b46ae
luisxxx1 <luisxxx103@gmail.com>
2026-01-17 22:16:41 -0500

    Revert "Correcion de rotas"
    This reverts commit 4c3f2bf699a12444b6a07563ea933a549e4625e0.

--- routes/web.php
+++ routes/web.php
@@ -44,12 +44,8 @@ Route::get('/sales/report/pdf', [TableController::class, 'downloadPDF'])->name('
 Route::middleware('guest')->group(function () {
     Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
     Route::post('/login', [LoginController::class, 'login']);
-
-    // Registro
-    Route::get('/register', [RegisterController::class, 'create'])->name('register'); //  formulario
-    Route::post('/register', [RegisterController::class, 'store']); // proceso de formulario
-
-    // Recuperación de contraseña
+    Route::get('/register', [RegisterController::class, 'create'])->name('register');
+    Route::post('/register', [RegisterController::class, 'register']);
     Route::get('/password/reset', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
     Route::post('/password/email', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
     Route::get('/password/reset/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
